4c48d256a4e3368b50e8a70249c8a88f6f9be3d9
poorniv-89 <poorniv.89@gmail.com>
2025-05-20 14:42:03 -0600

    Search results are fetched from API

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom"
-export default function BooksPage(){
+export default function BooksPage() {
     const [searchParams] = useSearchParams();
+    const [searchResults, setSearchResults]=useState(null);
     const query = searchParams.get('query');
-    return(
-        <h3>Search Resuts for {query}... </h3>
+
+    useEffect(() => {
+        async function getBooks() {
+            const API = 'https://openlibrary.org/search.json?subject=children+stories&title='
+            const res = await fetch(`${API}${query}`);
+            const data = await res.json();
+            console.log(data);
+        }
+        getBooks();
+    }, [query])
+
+
+    return (
+        <h2>Search Resuts for {query}... </h2>
     )
 }
