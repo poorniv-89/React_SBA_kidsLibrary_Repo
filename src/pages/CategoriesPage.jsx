313eca03b44fe3ca0b0846370c5b351c20db0274
poorniv-89 <poorniv.89@gmail.com>
2025-05-21 19:03:21 -0600

    Retrieved all subject category from API

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
-export default function CategoriesPage(){
 
-    useEffect(()=>{
-        async function getCategories(){
-            
-        }
-    },[])
+export default function CategoriesPage() {
+  useEffect(() => {
+    async function getCategories() {
+      const API = 'https://openlibrary.org/search.json?subject=children+stories&limit=20';
+      const res = await fetch(API);
+      const data = await res.json();
+      const books = data.docs;
 
-    return(
-        <h1>Categories page</h1>
-    )
+      for (const book of books) {
+        const key = book.key; 
+        const newAPI = `https://openlibrary.org${key}.json`;
+        const subjectRes = await fetch(newAPI);
+        const bookSubjects = await subjectRes.json();
+
+        console.log(bookSubjects.subjects);  
+      }
+    }
+    getCategories();
+  }, []);
+
+  return (
+    <h1>Categories page</h1>
+  );
 }
