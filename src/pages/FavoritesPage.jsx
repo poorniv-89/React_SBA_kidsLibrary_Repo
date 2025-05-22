3d9bd4a48bae18bb548e8e1ce5eb2ee8cba83eaa
poorniv-89 <poorniv.89@gmail.com>
2025-05-21 19:53:01 -0600

    Added comments to favorites and homepage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,3 +1,6 @@
+// stretch goals
+// In the future, this page will allow users to view and manage their favorite books, providing quick access to stories they love and want to revisit.
+
 export default function FavoritesPage(){
     return(
         <h1>Favorites page under construction!!!</h1>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
+// HomePage: This is the Main landing page of the app.
+// Displays the SearchBar component for users to search books.
+// Shows a heading for popular books of the week.
+// Renders the BooksGallery component to showcase a collection of popular books.
+
 import SearchBar from "../components/SearchBar"
 import BooksGallery from "../components/BooksGallery"
-export default function HomePage(){
-    return(
+export default function HomePage() {
+    return (
         <div>
-       <SearchBar/>
-       <h2>Popular this week</h2>
-       <BooksGallery/>
-
+            <SearchBar />
+            <h2>Popular this week</h2>
+            <BooksGallery />
         </div>
     )
 }
